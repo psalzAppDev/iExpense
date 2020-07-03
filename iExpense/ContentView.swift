428ec4a25a785f14bb519a36b1900f04fc445dc3
Peter Salz <psalz.app.dev@web.de>
2020-07-03 18:01:37 +0200

    Setup project

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,33 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationBarTitle("iExpense")
+        }
     }
 }
 
