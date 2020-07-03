994b7da308b3596314608fbdbba6476aad804762
Peter Salz <psalz.app.dev@web.de>
2020-07-03 22:34:28 +0200

    Challenge 1: Added Edit Button to List

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -73,7 +73,8 @@ struct ContentView: View {
                 .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
-            .navigationBarItems(trailing: Button(action: {
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
                 self.showingAddExpense = true
             }) {
                 
