ac3c7051b027ee74fcd95a8edb3998fc4c85b22e
Peter Salz <psalz.app.dev@web.de>
2020-07-03 18:17:28 +0200

    Added an AddView sheet

---
+++ iExpense/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Peter Salz on 03.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @ObservedObject var expenses: Expenses
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
