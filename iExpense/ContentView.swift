886e58d2aee3f813262ddd8ebf6fe3384eb9cd67
Peter Salz <psalz.app.dev@web.de>
2020-07-03 18:05:51 +0200

    Added row deletion

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -32,9 +32,25 @@ struct ContentView: View {
                 ForEach(expenses.items, id: \.name) { item in
                     Text(item.name)
                 }
+                .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing: Button(action: {
+                let expense = ExpenseItem(
+                    name: "Test",
+                    type: "Personal",
+                    amount: 5
+                )
+                self.expenses.items.append(expense)
+            }) {
+                
+                Image(systemName: "plus")
+            })
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
