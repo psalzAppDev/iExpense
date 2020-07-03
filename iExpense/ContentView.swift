e211f7925321b21e2092fc426afe00cd9933d258
Peter Salz <psalz.app.dev@web.de>
2020-07-03 18:00:47 +0200

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Peter Salz on 03.07.20.
+//  Copyright © 2020 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
